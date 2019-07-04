746c4b1d2916151f1f674d5b2a9b54e50991a3b2
lm-jira <jira@leapmind.io>
2019-07-04 18:46:56 +0900

    add export and predict option in run.py

--- run.py
+++ run.py
@@ -4,18 +4,20 @@ import importlib
 from tensorflow import keras
 
 @click.command()
-@click.option('--opt', type=click.Choice(['train', 'test']), default='train', 
+@click.option('--opt', type=click.Choice(['train', 'test', 'export', 'predict']), default='train', 
                   help='train or test the model')
-def run(opt):
+@click.option('--predict_img', type=click.Choice(['train', 'test', 'export', 'predict']), default='train', 
+                  help='train or test the model')
+def run(opt, predict_img):
     fashion_mnist = keras.datasets.fashion_mnist
+    mod_name = "executor.{}".format(opt)
+    mod = importlib.import_module(mod_name)
 
     if opt == "train":
         (train_images, train_labels), (_, _) = fashion_mnist.load_data()
         # preprocess images
         train_images = train_images / 255.0
 
-        mod_name = "executor.train"
-        mod = importlib.import_module(mod_name)
         mod.run(train_images, train_labels)
 
     elif opt == "test":
@@ -23,9 +25,13 @@ def run(opt):
         # preprocess images
         test_images = test_images / 255.0
 
-        mod_name = "executor.test"
-        mod = importlib.import_module(mod_name)
         mod.run(test_images, test_labels)
+
+    elif opt == "export":
+        mod.run()
+
+    elif opt == 'predict':
+        mod.run(predict_img)
 
 
 if __name__ == "__main__":
